5cec2cc660d9586be93428423728b46779041166
Singgih Saputro <singgihsap@gmail.com>
2018-02-24 20:16:25 +0700

    add component for navbar

---
+++ src/components/layouts/navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+    <div class="container">
+      <router-link v-bind:to="'/'" class="navbar-brand">minSomedia</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name:'componentNavbar'
+  }
+</script>
